5171daeac05abbbfa1caa804a6296d6cc1802225
Stef <devisdirect91@gmail.com>
2026-06-27 16:13:09 +0200

    Remove secondary CTA from landing hero

--- components/landing/hero.tsx
+++ components/landing/hero.tsx
@@ -106,12 +106,6 @@ export function Hero() {
               Commencer gratuitement
               <ArrowRight className="h-4 w-4 transition-transform group-hover:translate-x-0.5" />
             </Link>
-            <Link
-              href="#showcase"
-              className={buttonVariants({ size: "lg", variant: "secondary" })}
-            >
-              Voir des exemples
-            </Link>
           </motion.div>
 
           <motion.p
